19beacd6d4b2ae271dd69f3b23e7a006767fc3b9
Nick Poulden <nick@domine.co.uk>
2009-02-02 14:16:54 +0000

    Add start date and issue number integer conversions

--- lib/active_merchant/billing/credit_card.rb
+++ lib/active_merchant/billing/credit_card.rb
@@ -112,6 +112,9 @@ module ActiveMerchant #:nodoc:
       def before_validate #:nodoc: 
         self.month = month.to_i
         self.year  = year.to_i
+        self.start_month = start_month.to_i
+        self.start_year  = start_year.to_i
+        self.issue_number  = issue_number.to_i
         self.number = number.to_s.gsub(/[^\d]/, "")
         self.type.downcase! if type.respond_to?(:downcase)
         self.type = self.class.type?(number) if type.blank?
